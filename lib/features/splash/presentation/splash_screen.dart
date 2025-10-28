923de35da475f620988fbcbd1a89d2a5a4d25b72
Mohamed Emad <mohamedemad199912@gmail.com>
2025-10-28 15:42:20 +0300

    refactor splash

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -4,31 +4,38 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:to_do/core/style/colors/app_colors.dart';
 import 'package:to_do/generated/assets.dart';
-
 import '../../../core/routing/routes.dart';
 
-class SplashScreen extends StatelessWidget {
-
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToOnboarding();
+  }
+
+  Future<void> _navigateToOnboarding() async {
+    await Future.delayed(const Duration(milliseconds: 1500));
+    if (mounted) {
+      context.replace(Routes.onBoarding);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(const Duration(milliseconds: 1500), () {
-        if (context.mounted) {
-          context.replace(Routes.onBoarding);
-        }
-      });
-    });
-
 
     return Scaffold(
       backgroundColor: AppColors.nearBlack,
       body: Center(
         child: SvgPicture.asset(
-            Assets.svgsSplash
+          Assets.svgsSplash,
+          fit: BoxFit.contain,
         ),
       ),
     );
