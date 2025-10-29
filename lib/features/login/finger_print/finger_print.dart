58005b63fe325dafeb0d57dab246422d07d3403f
Mohamed Emad <mohamedemad199912@gmail.com>
2025-10-29 16:57:05 +0300

    complete finger_print

--- lib/features/login/finger_print/finger_print.dart
+++ lib/features/login/finger_print/finger_print.dart
@@ -1,27 +1,25 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../core/style/colors/app_colors.dart';
 import '../presentation/bloc/login_bloc.dart';
 
 class FingerPrint extends StatelessWidget {
-
   const FingerPrint({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return BlocBuilder<LoginBloc, LoginState>(
-
+      buildWhen: (previous, current) =>
+      current is BiometricAvailable || current is BiometricNotAvailable,
       builder: (context, state) {
-
         final bloc = context.read<LoginBloc>();
+        final biometricAvailable = bloc.biometricAvailable;
 
         return SizedBox(
           width: 60,
           height: 60,
           child: InkWell(
-            onTap: !state.biometricAvailable
+            onTap: !biometricAvailable
                 ? null
                 : () {
               bloc.add(BiometricAuthenticationRequested());
@@ -32,12 +30,12 @@ class FingerPrint extends StatelessWidget {
                 width: 60,
                 height: 60,
                 decoration: BoxDecoration(
-                  color: state.biometricAvailable
+                  color: biometricAvailable
                       ? AppColors.lavenderPurple[0]
                       : Colors.grey[200],
                   shape: BoxShape.circle,
                   border: Border.all(
-                    color: state.biometricAvailable
+                    color: biometricAvailable
                         ? AppColors.lavenderPurple
                         : Colors.grey,
                   ),
@@ -45,7 +43,7 @@ class FingerPrint extends StatelessWidget {
                 child: Icon(
                   Icons.fingerprint,
                   size: 32,
-                  color: state.biometricAvailable
+                  color: biometricAvailable
                       ? AppColors.lavenderPurple
                       : Colors.grey,
                 ),
