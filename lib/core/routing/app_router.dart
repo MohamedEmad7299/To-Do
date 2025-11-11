571f11f516438117d67d7583314576cfb2fa91eb
Mohamed Emad <mohamedemad199912@gmail.com>
2025-11-11 10:49:45 +0200

    add new routes

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:to_do/core/routing/routes.dart';
@@ -12,26 +13,27 @@ import '../../features/authentication/register/presentation/bloc/register_bloc.d
 import '../../features/authentication/register/presentation/register_screen.dart';
 import '../../features/bottom_nav_pages/home/presentation/bloc/home_bloc.dart';
 import '../../features/bottom_nav_pages/home/presentation/home_screen.dart';
+import '../../features/bottom_nav_pages/home/presentation/models/task_model.dart';
 import '../../features/bottom_nav_pages/profile/presentation/profile_screen.dart';
+import '../../features/bottom_nav_pages/profile/presentation/settings_screen.dart';
+import '../../features/bottom_nav_pages/tasks/presentation/task_details_screen.dart';
 import '../../features/splash/presentation/splash_screen.dart';
 import '../fire_base/auth_service.dart';
 
 class AppRouter {
   static final router = GoRouter(
-    initialLocation: Routes.onBoarding,
+    initialLocation: Routes.splash,
     routes: [
       GoRoute(
         path: Routes.splash,
-        builder: (context, state) =>
-        const SplashScreen(),
+        builder: (context, state) => const SplashScreen(),
       ),
       GoRoute(
         path: Routes.onBoarding,
-        builder: (context, state) =>
-            BlocProvider(
-              create: (context) => OnboardingBloc(totalPages: 3),
-              child: OnboardingScreen(),
-            ),
+        builder: (context, state) => BlocProvider(
+          create: (context) => OnboardingBloc(totalPages: 3),
+          child: OnboardingScreen(),
+        ),
       ),
       GoRoute(
         path: Routes.welcome,
@@ -39,40 +41,46 @@ class AppRouter {
       ),
       GoRoute(
         path: Routes.login,
-        builder: (context, state) =>
-            BlocProvider(
-              create: (context) => LoginBloc(authService: AuthService()),
-              child: LoginScreen(),
-            ),
+        builder: (context, state) => BlocProvider(
+          create: (context) => LoginBloc(authService: AuthService()),
+          child: LoginScreen(),
+        ),
       ),
       GoRoute(
         path: Routes.forgetPassword,
-        builder: (context, state) =>
-            BlocProvider(
-              create: (context) => ResetPasswordBloc(authService: AuthService()),
-              child: ResetPasswordScreen(),
-            ),
+        builder: (context, state) => BlocProvider(
+          create: (context) => ResetPasswordBloc(authService: AuthService()),
+          child: ResetPasswordScreen(),
+        ),
       ),
       GoRoute(
         path: Routes.home,
-        builder: (context, state) =>
-            BlocProvider(
-              create: (context) => HomeBloc(),
-              child: HomeScreen(),
-            ),
+        builder: (context, state) => BlocProvider(
+          create: (context) => HomeBloc(),
+          child: const HomeScreen(),
+        ),
       ),
       GoRoute(
         path: Routes.register,
-        builder: (context, state) =>
-            BlocProvider(
-              create: (context) => RegisterBloc(authService: AuthService()),
-              child: RegisterScreen(),
-            ),
+        builder: (context, state) => BlocProvider(
+          create: (context) => RegisterBloc(authService: AuthService()),
+          child: RegisterScreen(),
+        ),
       ),
       GoRoute(
         path: Routes.profile,
-        builder: (context, state) =>
-            ProfilePage(),
+        builder: (context, state) => const ProfilePage(),
+      ),
+      GoRoute(
+        path: Routes.settings,
+        builder: (context, state) => const SettingsScreen(),
+      ),
+      GoRoute(
+        path: Routes.taskDetails,
+        builder: (context, state) {
+          final task = state.extra as TaskModel;
+          return TaskDetailsScreen(task: task);
+        },
       ),
     ],
   );
