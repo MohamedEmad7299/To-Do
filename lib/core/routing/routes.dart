8d030168ede2322b2573bb31ed6b8d2ace9c436f
Mohamed Emad <mohamedemad199912@gmail.com>
2025-11-20 12:53:02 +0200

    add new routes

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -11,4 +11,11 @@ class Routes{
   static const String profile = '/profile';
   static const String settings = '/settings';
   static const String taskDetails = '/task_details';
+  static const String changeAccountName = '/change_account_name';
+  static const String changePassword = '/change_password';
+  static const String changeAccountImage = '/change_account_image';
+  static const String aboutUs = '/about_us';
+  static const String faq = '/faq';
+  static const String help = '/help';
+  static const String support = '/support';
 }
