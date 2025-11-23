dd6b4eb15efd955c902c6acab0a51f761c558149
Mohamed Emad <mohamedemad199912@gmail.com>
2025-11-23 14:00:06 +0200

    refactor add primaryColor & localization

--- lib/features/on_boarding/data/onboarding_data.dart
+++ lib/features/on_boarding/data/onboarding_data.dart
@@ -1,24 +1,28 @@
 
 
+import 'package:flutter/material.dart';
+import 'package:to_do/l10n/app_localizations.dart';
 import '../../../generated/assets.dart';
 import '../presentation/models/on_boarding_model.dart';
 
 class OnboardingData {
-  static const List<OnboardingPageModel> pages = [
-    OnboardingPageModel(
-      image: Assets.svgsBoard1,
-      title: 'Manage your tasks',
-      description: 'You can easily manage all of your daily tasks in DoMe for free',
-    ),
-    OnboardingPageModel(
-      image: Assets.svgsBoard2,
-      title: 'Create daily routine',
-      description: 'In ToDo you can create your personalized routine to stay productive',
-    ),
-    OnboardingPageModel(
-      image: Assets.svgsBoard3,
-      title: 'Organize your tasks',
-      description: 'You can organize your daily tasks by adding your tasks into separate categories',
-    ),
-  ];
+  static List<OnboardingPageModel> getPages(BuildContext context) {
+    return [
+      OnboardingPageModel(
+        image: Assets.svgsBoard1,
+        title: AppLocalizations.of(context)!.onboarding1Title,
+        description: AppLocalizations.of(context)!.onboarding1Description,
+      ),
+      OnboardingPageModel(
+        image: Assets.svgsBoard2,
+        title: AppLocalizations.of(context)!.onboarding2Title,
+        description: AppLocalizations.of(context)!.onboarding2Description,
+      ),
+      OnboardingPageModel(
+        image: Assets.svgsBoard3,
+        title: AppLocalizations.of(context)!.onboarding3Title,
+        description: AppLocalizations.of(context)!.onboarding3Description,
+      ),
+    ];
+  }
 }
